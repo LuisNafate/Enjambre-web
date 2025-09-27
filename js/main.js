0f82674d41b35ae5903bea158464b1b7ee52e854
Luis Nafate <luisnafate51@gmail.com>
2025-09-27 00:08:02 -0600

    Refactor lógica de carga de páginas en main.js y mejora de la función loadHomePage

--- js/main.js
+++ js/main.js
@@ -3,12 +3,11 @@ import { getArtist, getAlbums, getTopTracks } from './spotify-api.js';
 
 const artistId = '1ZdhAl62G6ZlEKqIwUAfZR'; // ID Enjambre
 
-// Obtenemos la ruta del archivo actual para saber en qué página estamos
 const currentPage = window.location.pathname.split("/").pop();
 
 // Este evento se dispara cuando el contenido del HTML ha sido cargado
 document.addEventListener('DOMContentLoaded', () => {
-    // Verificamos en qué página estamos y ejecutamos el código correspondiente
+  
     if (currentPage === 'index.html' || currentPage === '') {
         loadHomePage();
     } else if (currentPage === 'albumes.html') {
@@ -17,3 +16,18 @@ document.addEventListener('DOMContentLoaded', () => {
         loadTopTracksPage();
     }
 });
+
+// Función para cargar la página de inicio
+const loadHomePage = async () => {
+    const artist = await getArtist(artistId);
+    const artistInfoDiv = document.getElementById('artist-info');
+
+    // Creamos el HTML con los datos del artista
+    artistInfoDiv.innerHTML = `
+        <h1>${artist.name}</h1>
+        <img src="${artist.images[0].url}" alt="${artist.name}" width="300">
+        <p><strong>Géneros:</strong> ${artist.genres.join(', ')}</p>
+        <p><strong>Popularidad:</strong> ${artist.popularity} / 100</p>
+        <p><strong>Seguidores en Spotify:</strong> ${artist.followers.total.toLocaleString()}</p>
+    `;
+};
